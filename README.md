532a50d3e3026a605f9442a696e7ae4bd4868a1d
Duane Curlee <duane.curlee@gmail.com>
2019-04-12 13:55:24 -0400

    Updated to prevent over-writing existing photos

--- README.md
+++ README.md
@@ -1,8 +1,14 @@
 # photo-importer
 Python tool to import photos into a structured folder archive
 
-This tool will copy only \*.jpg and \*.jpeg files from a given source directory and put them into a photo archive. My choice of archive is $HOME\Photos
+This tool will copy only \*.jpg and \*.jpeg files from a given
+source directory and put them into a photo archive. My choice
+of archive folder is $HOME\Photos
 
-The Photos are arranged in a structure of Photos\(4-digit year)\(2-digit month)\(2-digit date)\(original file name) - much like the Gnome Desktop application Shotwell arranged them.
+The Photos are arranged in a folder structure of:
+$HOME\Photos\(4-digit year)\(2-digit month)\(2-digit date)\(original file name)
 
-This script does not delete the files from the source.
+just like the Gnome Desktop application Shotwell arranged them.
+
+This script does not delete the files from the source. If a file is already
+in the archive, it will not be copied.
--- photo-import-cli.py
+++ photo-import-cli.py
@@ -1,31 +1,37 @@
 #!/usr/bin/python
-
+"""
+This utility copies photos from a given folder to your home archive.
+"""
 import shutil, sys, os, datetime
 photo_home = os.path.expanduser('~') + os.path.sep + 'Photos-testing' + os.path.sep
-if not os.path.exists(photo_home):
-    os.makedirs(photo_home)
 
 if len(sys.argv) < 2:
     print('Exiting, a command-line folder name is required.')
 else:
+    if not os.path.exists(photo_home):
+        print('Does not exist, creating: ' + photo_home)
+        os.makedirs(photo_home)
     for arg in sys.argv[1:]:
         if os.path.isdir(arg) == True:
             for root, dirs, files in os.walk(arg):
                 print("Checking folder: " + root + os.path.sep)
                 for name in files:
                     if name[-4:].lower()  == ".jpg" or name[-5:].lower() == ".jpeg":
-#                        print("%s last modified: %s" % (name, time.ctime(os.path.getmtime(root + os.path.sep + name))))
-#                        print("%s created: %s"       % (name, time.ctime(os.path.getctime(root + os.path.sep + name))))
-                        the_dt = datetime.datetime.fromtimestamp(os.path.getmtime(root + os.path.sep + name))
+                        the_source = root + os.path.sep + name
+                        the_dt = datetime.datetime.fromtimestamp(os.path.getmtime(the_source))
                         the_path = the_dt.strftime('%Y') + os.path.sep
                         the_path += the_dt.strftime('%m') + os.path.sep
                         the_path += the_dt.strftime('%d') + os.path.sep
                         the_path = photo_home + the_path
+                        the_target = the_path + name
                         if not os.path.exists(the_path):
                             os.makedirs(the_path)
-                        print('Copying %s to: %s' % (root + os.path.sep + name, the_path + name))
-                        shutil.copy2(root + os.path.sep + name, the_path)
+                        if os.path.exists(the_target):
+                            print('Not copying, already in archive: ' + the_source)
+                        else:
+                            print('Copying %s to: %s' % (the_source, the_target))
+                            shutil.copy2(the_source, the_path)
                     else:
-                        print('Not a JPEG file, skipping: %s' % name)
+                        print('Not a JPEG file, skipping: %s' % root + os.path.sep + name)
         else:
             print('Not a folder, skipping: ' + arg)
